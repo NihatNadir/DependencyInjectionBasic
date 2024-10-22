197a848c7b325893223697ad7ad12f603f12c4ae
Nihat Nadir Börlü <134305591+NihatNadir@users.noreply.github.com>
2024-10-22 21:46:52 +0300

    Program.cs

--- DependencyInjectionBasic/Program.cs
+++ DependencyInjectionBasic/Program.cs
@@ -2,10 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ITeacher, Teacher>(); // Yaşam döngüsü
 
 var app = builder.Build();
 
